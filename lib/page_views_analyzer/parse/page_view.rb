77be297e25da2c7903dfa0cf25316a5717f00d1c
spatle <sandeep.patle@heyjobs.de>
2021-03-09 19:09:56 +0100

    split line and parse path and ip

--- lib/page_views_analyzer/parse/page_view.rb
+++ lib/page_views_analyzer/parse/page_view.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
 
+# class to parse path and ip from line from log file
 module PageViewsAnalyzer
   module Parse
     class PageView
-      def initialize(string_row)
+      def initialize(line_string)
+        @line_string = line_string
       end
 
       def call
+        parse_and_assign_data
+
+        [page_path, ip]
+      end
+
+      private
+
+      attr_reader :line_string
+      attr_accessor :page_path, :ip
+
+      def parse_and_assign_data
+        @page_path = parse_data[0]
+        @ip = parse_data[1]
+      end
+
+      def parse_data
+        @parse_data ||= line_string.split.map(&:strip)
       end
     end
   end
